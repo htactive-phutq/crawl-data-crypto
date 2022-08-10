86d3a036577bf758c6550944a94e82eb91f7373b
linhnguyen <linh.nguyen@stunited.vn>
2022-08-10 18:16:51 +0700

    improve perfomance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { makeStyles } from "@material-ui/core";
-import Homepage from "./Pages/HomePage";
+// import Homepage from "./Pages/HomePage";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
-import CoinPage from "./Pages/CoinPage";
+import React, { lazy, Suspense } from 'react'
+// import CoinPage from "./Pages/CoinPage";
 import Header from "./components/Header";
 
+const Homepage = lazy(() => import('./Pages/HomePage'))
+const CoinPage = lazy(() => import('./Pages/CoinPage'))
 const useStyles = makeStyles(() => ({
   App: {
     backgroundColor: "#f59ebd",
@@ -20,8 +23,10 @@ function App() {
     <BrowserRouter>
       <div className={classes.App}>
         <Header />
-        <Route path="/" component={Homepage} exact />
-        <Route path="/coins/:id" component={CoinPage} exact />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path="/" component={Homepage} exact />
+          <Route path="/coins/:id" component={CoinPage} exact />
+        </Suspense>
       </div>
     </BrowserRouter>
   );
